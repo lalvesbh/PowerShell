6b9e71267e8f7cf75f7345a2b27a2ea5317e7e73
lalvesbh <47647728+lalvesbh@users.noreply.github.com>
2020-11-05 19:42:30 -0300

    Add files via upload

---
+++ Funcao.ps1
@@ -0,0 +1,3 @@
+﻿Function Meu-HD {
+Get-WmiObject Win32_LogicalDisk -Filter DriveType=3 | Select-Object DeviceID, @{'Name'='Tamanho (GB)'; 'Expression'={[math]::truncate($_.size /1GB)}}, @{'Name' = 'Espaço Livre (GB)'; 'Expression' ={[math]::truncate($_.freespace /1GB)}}
+}
---
+++ Funcao_v2.ps1
@@ -0,0 +1,7 @@
+﻿Function somar{
+
+param($a, $b)
+$resultdato = $a + $b
+Write-Host "A resposta é $resultdato"
+
+}
