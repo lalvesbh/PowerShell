8c0eea036820d82985220c495cc282917d2c8623
lalvesbh <47647728+lalvesbh@users.noreply.github.com>
2020-11-05 19:18:35 -0300

    Add files via upload

---
+++ Selec-string.ps1
@@ -0,0 +1,12 @@
+﻿Get-Content '.\UPN GTI.txt' | Select-String 777, 88, 90
+
+Select-String -Pattern 10 '.\UPN GTI.txt'
+
+Get-ChildItem .\*.txt | Select-String -Pattern 903164
+
+
+Select-String -Path '.\users - Copia.txt' -Pattern 903164 -Context 1,1
+
+
+Select-String -Path '.\users - Copia.txt' -Pattern 903164 -NotMatch
+cls
